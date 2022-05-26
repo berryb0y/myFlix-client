006e5cf9e4e6527847868edaa4d5ee80bdef1079
berryb0y <ab4537@gmail.com>
2022-05-25 20:44:50 -0500

    axios

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import axios from 'axios';
+
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
@@ -7,13 +9,21 @@ export class MainView extends React.Component {
     constructor(){
         super();
         this.state = {
-            movies: [
-                { _id: 1, Title: 'Iron Man', Description: 'desc1...', ImagePath: '...'},
-                { _id: 2, Title: 'The Incredible Hulk', Description: 'desc2...', ImagePath: '...'},
-                { _id: 3, Title: 'Iron Man 2', Description: 'desc3...', ImagePath: '...'}
-            ],
+            movies: [],
             selectedMovie: null
         }
+    }
+
+    componentDidMount(){
+        axios.get('https://berry-node-api.herokuapp.com/movies')
+            .then(response => {
+                this.setState({
+                    movies: response.data
+                });
+            }) 
+            .catch(error => {
+                console.log(error);
+            });
     }
 
     setSelectedMovie(newSelectedMovie) {
@@ -34,12 +44,11 @@ export class MainView extends React.Component {
         {selectedMovie
           ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
           : movies.map(movie => (
-            <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }}/>
+            <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }}/>
           ))
         }
       </div>
     );
   }
-
 }
 export default MainView
