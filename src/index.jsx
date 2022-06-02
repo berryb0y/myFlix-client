5d11ae739f10d7a88e49867cd9a875afafdaa46b
berryb0y <ab4537@gmail.com>
2022-06-01 20:05:56 -0500

    .

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { MainView } from './components/main-view/main-view';
 
 //import statement to indicate that you need to bundle `./index.scss`
@@ -18,4 +18,5 @@ class MyFlixApplication extends React.Component {
 const container = document.getElementsByClassName('app-container')[0];
 
 //Tells react to render your app in the root DOM element
-root.render(React.createElement(MyFlixApplication))
+const root = ReactDOM.createRoot(container);
+root.render(<MyFlixApplication/>);
